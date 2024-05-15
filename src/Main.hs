34ef720391f6e70ba66c3c84938e3b57829e2dd9
nkarl <21993921+nkarl@users.noreply.github.com>
2024-05-15 16:20:18 -0700

    Updated.

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,10 @@ module Main where
 
 import Kick.Rock
 import Touch.Grass
+import FizzBuzz.FizzBuzz
 
 main :: IO ()
 main = do
   --Kick.Rock.test
-  Touch.Grass.test
+  --Touch.Grass.test
+  FizzBuzz.FizzBuzz.test
