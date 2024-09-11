d983c39f6387e13c9335100ec68c9d31b44c4912
nkarl <21993921+nkarl@users.noreply.github.com>
2024-09-11 14:31:07 -0700

    Reviewed the Parser monad.

--- src/Library/Monads/Parser.hs
+++ src/Library/Monads/Parser.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE MonoLocalBinds #-}
+
 module Library.Monads.Parser where
 
 -- import Control.Applicative (Alternative, empty, (<|>))
@@ -13,108 +14,112 @@ import Prelude
 
 type State a = (String, a)
 
-type Proc e a = (Fail e) => String -> Either e (State a)
+type Action e a = (Failable e) => String -> Either e (State a)
 
-newtype Parse e a = Parse (Proc e a)
+newtype ParseM e a = ParseM (Action e a)
 
 data ErrorMsg
   = EOF
   | InvalidChar String
   deriving (Show)
 
-class Fail (e :: Type) where
+class Failable (e :: Type) where
   eof :: e
-  invalidChar :: String -> e
+  invalidSymbol :: String -> e
 
-instance Fail ErrorMsg where
+instance Failable ErrorMsg where
   eof = EOF
-  invalidChar = InvalidChar
+  invalidSymbol = InvalidChar
 
-instance Functor (Parse e) where
-  fmap f (Parse mx) = Parse (fmap (fmap f) . mx)
+instance Functor (ParseM e) where
+  fmap f (ParseM mx) = ParseM $ ((f <$>) <$>) . mx
 
-instance Applicative (Parse e) where
-  (<*>) (Parse mf) (Parse mx) =
-    Parse $ \s -> do
+instance Applicative (ParseM e) where
+  (<*>) (ParseM mf) (ParseM mx) =
+    ParseM $ \s -> do
       (s1, f) <- mf s
       (s2, x) <- mx s1
       pure (s2, f x)
-  pure x = Parse $ \s -> Right (s, x)
+  pure x = ParseM $ \s -> Right (s, x)
 
-instance Monad (Parse e) where
-  (>>=) (Parse mx) f =
-    Parse $ \s -> do
+instance Monad (ParseM e) where
+  (>>=) (ParseM mx) f =
+    ParseM $ \s -> do
       (s', x) <- mx s
-      Parse g <- pure $ f x
+      ParseM g <- pure $ f x
       g s'
 
-char :: forall e. Parse e Char
-char = Parse $ \s -> case uncons s of
+char :: forall e. ParseM e Char
+char = ParseM $ \s -> case uncons s of
   Nothing -> Left eof
   Just (c, rest) -> Right (rest, c)
 
-twoChars :: forall e. Parse e (Char, Char)
-twoChars =
-  -- (,) <$> char <*> char
-  do
-    c1 <- char
-    c2 <- char
-    pure (c1, c2)
+type Pair = (Char, Char)
 
--- threeChars :: forall e. Parse String e (Char, (Char, Char))
+twoChars :: forall e. ParseM e Pair
+twoChars =
+  -- do
+  --    c1 <- char
+  --    c2 <- char
+  --    pure (c1, c2)
+  (,) <$> char <*> char
+
+-- threeChars :: forall e. ParseM String e (Char, (Char, Char))
 -- threeChars = (,) <$> char <*> twoChars
 
-threeChars :: forall e. Parse e String
+threeChars :: forall e. ParseM e String
 threeChars =
-  -- (\c1 c2 c3 -> [c1, c2, c3]) <$> char <*> char <*> char
-  do
-    c1 <- char
-    c2 <- char
-    c3 <- char
-    pure [c1, c2, c3]
+  -- do
+  --    c1 <- char
+  --    c2 <- char
+  --    c3 <- char
+  --    pure [c1, c2, c3]
+  (\c1 c2 c3 -> [c1, c2, c3]) <$> char <*> char <*> char
 
-unwrap :: forall e a. (Fail e) => Parse e a -> Proc e a
-unwrap (Parse f) = f
+unwrap :: forall e a. ParseM e a -> Action e a
+unwrap (ParseM f) = f
 
-parse :: Parse ErrorMsg a -> Proc ErrorMsg a
+parse :: ParseM ErrorMsg a -> Action ErrorMsg a
 parse = unwrap
 
-fail' :: forall e a. (Fail e) => e -> Parse e a
-fail' e = Parse $ \_ -> Left e
+failedParsing :: forall e a. (Failable e) => e -> ParseM e a
+failedParsing e = ParseM $ \_ -> Left e
 
-satisfy :: forall e. (Fail e) => String -> (Char -> Bool) -> Parse e Char
-satisfy expected predicate =
+satisfy :: forall e. (Failable e) => (Char -> Bool) -> String -> ParseM e Char
+satisfy predicate reason =
   char >>= \c ->
-    if predicate c then pure c else fail' $ invalidChar expected -- Left $ invalidChar expected
+    if predicate c
+      then pure c
+      else failedParsing $ invalidSymbol reason -- Left $ invalidSymbol expected
 
-digit :: forall e. (Fail e) => Parse e Char
-digit = satisfy "digit" (isDigit . fromChar)
+digit :: forall e. (Failable e) => ParseM e Char
+digit = satisfy (isDigit . fromChar) "is not a digit."
 
-letter :: forall e. (Fail e) => Parse e Char
-letter = satisfy "letter" (isAlpha . fromChar)
+letter :: forall e. (Failable e) => ParseM e Char
+letter = satisfy (isAlpha . fromChar) "is not a letter."
 
--- alphaNum :: forall e. (Fail e) => Parse e Char
--- alphaNum = Parse $ \s -> case (parse letter s :: Either ErrorMsg (State Char)) of
+-- alphaNum :: forall e. (Failable e) => ParseM e Char
+-- alphaNum = ParseM $ \s -> case (parse letter s :: Either ErrorMsg (State Char)) of
 -- Right x -> Right x
 -- Left _ -> case unwrap digit s of
 -- Left err -> Left err
 -- Right y -> Right y
-instance Alternative (Parse e) where
+instance Alternative (ParseM e) where
   (<|>) p1 p2 =
-    Parse $ \s -> case unwrap p1 s of
+    ParseM $ \s -> case unwrap p1 s of
       Left _ -> unwrap p2 s
       Right x -> Right x
 
-alphaNum :: forall e. (Fail e) => Parse e Char
-alphaNum = letter <|> digit <|> fail' (invalidChar "alphaNum")
+alphaNum :: forall e. (Failable e) => ParseM e Char
+alphaNum = letter <|> digit <|> failedParsing (invalidSymbol "is not an alphaNumeral.")
 
---count :: (Traversable f) => (Foldable f) => Int -> Parse e a -> Parse e (f a)
---count n p
-  -- | n <= 0 = pure mempty
-  -- | otherwise = sequence (replicate n p)
+-- count :: (Traversable f) => (Foldable f) => Int -> ParseM e a -> ParseM e (f a)
+-- count n p
+--    | n <= 0 = pure mempty
+--    | otherwise = sequence (replicate n p)
 
---count' :: forall e. Int -> Parse e Char -> Parse e String
---count' n p = ([]) <$> count n p
+-- count' :: forall e. Int -> ParseM e Char -> ParseM e String
+-- count' n p = ([]) <$> count n p
 
 test :: IO ()
 test = do
@@ -123,8 +128,9 @@ test = do
   print (parse char s :: Either ErrorMsg (State Char))
   print (parse twoChars s :: Either ErrorMsg (State (Char, Char)))
   print (parse threeChars s :: Either ErrorMsg (State String))
-  --print $ parse (count' 3 digit) "1234567"
-  --print $ parse (count' 3 digit) "abc1234"
-  --print $ parse (count' 4 letter) "Freddy"
-  --print $ parse (count' 10 alphaNum) "a1b2c3d4e5"
-  --print $ parse (count' 10 alphaNum) "######"
+
+-- print $ parse (count' 3 digit) "1234567"
+-- print $ parse (count' 3 digit) "abc1234"
+-- print $ parse (count' 4 letter) "Freddy"
+-- print $ parse (count' 10 alphaNum) "a1b2c3d4e5"
+-- print $ parse (count' 10 alphaNum) "######"
