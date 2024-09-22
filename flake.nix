5b013937a238e46f0694f1863d67dfad30bf9d98
nkarl <21993921+nkarl@users.noreply.github.com>
2024-09-21 22:52:53 -0700

    Updated Nix flake to use binary cache from IOG and NixOS to speed up build time.

--- flake.nix
+++ flake.nix
@@ -1,31 +1,72 @@
 {
-  description = "A Nix flake for Haskell development environment";
+  description = "A Nix flake for a Haskell development environment.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }: let
-    
-    system = "x86_64-linux";
+  outputs = { self, nixpkgs, flake-utils, haskellNix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
 
-  in {
-    devShells."${system}".default = let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
+    let
+      overlays = [
+        haskellNix.overlay (final: prev: {
+          haskell-snippets = final.haskell-nix.project' {
+            src = ./.;
+            compiler-nix-name = "ghc966";
 
-    in pkgs.mkShell {
-      packages = with pkgs; [
-        cabal-install
-        stack
-        haskell.compiler.ghc8107
-        haskell-language-server
+            shell.tools = {
+              cabal = {};
+              hlint = {};
+              stack = "3.1.1";
+              haskell-language-server = {};
+            };
+
+            shell.buildInputs = with pkgs; [
+              nixpkgs-fmt
+            ];
+          };
+        })
       ];
 
-      shellHook = ''
-        echo "ghc `${pkgs.haskell.compiler.ghc8107}/bin/ghc --version`"
-      '';
-    };
-  };
-}
+      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+
+      flake = pkgs.haskell-snippets.flake { };
+
+    in
+      flake // { packages.default = flake.packages."snippets:exe:snippets"; });
+}      
+
+#{
+  #description = "A Nix flake for Haskell development environment";
+
+  #inputs = {
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  #};
+
+  #outputs = { self, nixpkgs, ... }: let
+    
+    #system = "x86_64-linux";
+
+  #in {
+    #devShells."${system}".default = let
+      #pkgs = import nixpkgs {
+        #inherit system;
+      #};
+
+    #in pkgs.mkShell {
+      #packages = with pkgs; [
+        #cabal-install
+        #stack
+        #haskell.compiler.ghc8107
+        #haskell-language-server
+      #];
+
+      #shellHook = ''
+        #echo "ghc `${pkgs.haskell.compiler.ghc8107}/bin/ghc --version`"
+      #'';
+    #};
+  #};
+#}
