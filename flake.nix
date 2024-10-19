d617f2ed19e72a937faf82672fefa942d587f9a5
nkarl <21993921+nkarl@users.noreply.github.com>
2024-10-19 13:50:19 -0700

    Reverted to a simpler Nix flake. LSP doesn't work with IOG template and I don't have time to investigate right now.

--- flake.nix
+++ flake.nix
@@ -1,72 +1,37 @@
 {
-  description = "A Nix flake for a Haskell development environment.";
+  description = "A Nix flake for Haskell development environment";
 
   inputs = {
-    haskellNix.url = "github:input-output-hk/haskell.nix";
-    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils, haskellNix, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-
+  outputs = { self, nixpkgs, ... }:
     let
-      overlays = [
-        haskellNix.overlay (final: prev: {
-          haskell-snippets = final.haskell-nix.project' {
-            src = ./.;
-            compiler-nix-name = "ghc966";
 
-            shell.tools = {
-              cabal = {};
-              hlint = {};
-              stack = "3.1.1";
-              haskell-language-server = {};
-            };
-
-            shell.buildInputs = with pkgs; [
-              nixpkgs-fmt
-            ];
-          };
-        })
-      ];
-
-      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-
-      flake = pkgs.haskell-snippets.flake { };
+      system = "x86_64-linux";
 
     in
-      flake // { packages.default = flake.packages."snippets:exe:snippets"; });
-}      
+    {
+      devShells."${system}".default =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          ghc_ver = "ghc966";
 
-#{
-  #description = "A Nix flake for Haskell development environment";
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            cabal-install
+            haskell.compiler.${ghc_ver}
+            haskell-language-server
+            hlint
+          ];
 
-  #inputs = {
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  #};
-
-  #outputs = { self, nixpkgs, ... }: let
-    
-    #system = "x86_64-linux";
-
-  #in {
-    #devShells."${system}".default = let
-      #pkgs = import nixpkgs {
-        #inherit system;
-      #};
-
-    #in pkgs.mkShell {
-      #packages = with pkgs; [
-        #cabal-install
-        #stack
-        #haskell.compiler.ghc8107
-        #haskell-language-server
-      #];
-
-      #shellHook = ''
-        #echo "ghc `${pkgs.haskell.compiler.ghc8107}/bin/ghc --version`"
-      #'';
-    #};
-  #};
-#}
+          shellHook = ''
+            echo "ghc `${pkgs.haskell.compiler.${ghc_ver}}/bin/ghc --version`"
+          '';
+        };
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+    };
+}
